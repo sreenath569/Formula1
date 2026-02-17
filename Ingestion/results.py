eaeca1d919f5d654164742f3dcfa576acfbc910c
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-17 21:18:39 +0530

    Update results.py

--- Ingestion/results.py
+++ Ingestion/results.py
@@ -1,1 +1,43 @@
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+from pyspark.sql.functions import current_timestamp, col
 
+results_schema = StructType([
+  StructField("resultId" IntegerType(), False),
+  StructField("raceId" IntegerType(), True),
+  StructField("driverId" IntegerType(), True),
+  StructField("constructorId" IntegerType(), True),
+  StructField("number" IntegerType(), True),
+  StructField("grid" IntegerType(), True),
+  StructField("position" IntegerType(), True),
+  StructField("positionText" StringType(), True),
+  StructField("positionOrder" IntegerType(), True),
+  StructField("points" FloatType(), True),
+  StructField("laps" IntegerType(), True),
+  StructField("time" StringType(), True),
+  StructField("milliseconds" IntegerType(), True),
+  StructField("fastestLap" IntegerType(), True),
+  StructField("rank" IntegerType(), True),
+  StructField("fastestLapTime" StringType(), True),
+  StructField("fastestLapSpeed" FloatType(), True),
+  StructField("statusId" StringType(), True)
+])
+
+results_df = spark.read.schema(results_schema).json("path")
+
+results_with_columns_df = results_df.withColumnRenamed("resultId", "result_id") \
+                                    .withColumnRenamed("raceId", "race_id") \
+                                    .withColumnRenamed("driverId", "driver_id") \
+                                    .withColumnRenamed("constructorId", "constructor_id") \
+                                    .withColumnRenamed("positionText", "position_text") \
+                                    .withColumnRenamed("positionOrder", "position_order") \
+                                    .withColumnRenamed("fastestLap", "fastest_lap") \
+                                    .withColumnRenamed("fastestLapTime", "fastest_lap_time") \
+                                    .withColumnRenamed("fastestLapSpeed", "fastest_lap_speed") \
+                                    .withColumn("ingestion_date", current_timestamp())
+
+results_final_df = results_with_columns_df.drop(col("statusId"))
+
+results_final_df.write.mode("overwrite").parquet("path")
+
+                                                
+                                    
