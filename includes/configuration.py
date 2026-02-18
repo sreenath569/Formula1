190a44ee831f728c43cf6014ea6c97304c56c81e
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-18 11:52:48 +0530

    Update configuration.py

--- includes/configuration.py
+++ includes/configuration.py
@@ -1,1 +1,3 @@
-
+raw_folder_path = 'abfss://raw@formula1.dfs.core.windows.net'
+processed_folder_path = 'abfss://processed@formula1.dfs.core.windows.net'
+presentation_folder_path = 'abfss://presentation@formula1.dfs.core.windows.net'
