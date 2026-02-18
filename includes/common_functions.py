654ae0c684c09683d753a49b300e618dd43a7f0d
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-18 12:01:59 +0530

    Update common_functions.py

--- includes/common_functions.py
+++ includes/common_functions.py
@@ -1,1 +1,5 @@
+from pyspark.sql.functions import current_timestamp
 
+def add_ingestion_date(input_df):
+  output_df = input_df.withCoulmn("ingestion_date", current_timestamp())
+  return output_df 
