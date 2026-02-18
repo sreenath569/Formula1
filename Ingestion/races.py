d28431fcbb53442998330d779e66be1b01485efa
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-18 19:32:24 +0530

    Update races.py

--- Ingestion/races.py
+++ Ingestion/races.py
@@ -1,1 +1,40 @@
 
+dbutils.widgets.text("p_data_source", "")
+v_data_source = dbutils.widgets.get("p_data_source")
+
+%run "../includes/configuration"
+  
+%run "../includes/common_functions"
+
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
+from pyspark.sql.functions import col, lit
+
+# defining schema
+races_schema = StructType([
+  StructField("raceId" IntegerType(), False),
+  StructField("year" IntegerType(), True),
+  StructField("round" IntegerType(), True),
+  StructField("circuitId" IntegerType(), True),
+  StructField("name" StringType(), True),
+  StructField("date" DateType(), True),
+  StructField("time" StringType(), True),
+  StructField("url" StringType(), True)
+])
+
+# create dataframe of data file csv
+races_df = spark.read.option("header", True) \
+                      .schema(races_schema) \
+                      .csv(f"{raw_folder_path}/races.csv")
+
+races_with_timestamp_df = races_df.withColumn("race_timestamp", to_timestamp(concat(col("date"), lit(" "), col("time"), "yyyy-MM-dd HH:mm:ss")))
+
+races_selected_df = races_with_timestamp_df.select(col("raceId").alias("race_id"),col("year").alias("race_year"),col("round"),col("circuitId").alias("circuit_id"),col("name"),col("ingestion_date"),col("race_timestamp"))
+
+races_selected_df = races_selected_df.withColumn("data_source", lit(v_data_source))
+
+races_final_df = add_ingestion_date(races_selected_df)
+
+races_final_df.write.mode("overwrite").parquet("f"{processed_folder_path}/races")
+
+                                                
+                                    
