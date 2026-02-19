eec6a9a7661af95a773c9d87179ef525486cb0a4
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-19 20:48:55 +0530

    Update lap_times.py

--- Ingestion/lap_times.py
+++ Ingestion/lap_times.py
@@ -1,3 +1,11 @@
+
+dbutils.widgets.text("p_data_source", "")
+v_data_source = dbutils.widgets.get("p_data_source")
+
+%run "../includes/configuration"
+  
+%run "../includes/common_functions"
+
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from pyspark.sql.functions import current_timestamp
 
@@ -13,13 +21,14 @@ lap_times_schema = StructType([
 
 # create dataframe of data file multiline Json
 lap_times_df = spark.read.schema(lap_times_schema) \
-                        .csv("folderpath")
+                        .csv(f"{raw_folder_path}/lap_times.csv")
 
-lap_times_final_df = lap_times_df.withColumnRenamed("raceId", "race_id") \
-                               .withColumnRenamed("driverId", "driver_id") \
-                               .withColumn("ingestion_date", current_timestamp())
+lap_times_renamed_df = lap_times_df.withColumnRenamed("raceId", "race_id") \
+                               .withColumnRenamed("driverId", "driver_id")
 
-lap_times_final_df.write.mode("overwrite").parquet("folderpath")
+lap_times_final_df = add_ingestion_date(lap_times_renamed_df)
+
+lap_times_final_df.write.mode("overwrite").parquet("f"{processed_folder_path}/lap_times")
 
                                                 
                                     
