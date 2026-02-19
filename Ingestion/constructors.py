5c400be471071568bb0c1c74deeb844a95334a07
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-19 20:11:02 +0530

    Update constructors.py

--- Ingestion/constructors.py
+++ Ingestion/constructors.py
@@ -1,1 +1,30 @@
 
+dbutils.widgets.text("p_data_source", "")
+v_data_source = dbutils.widgets.get("p_data_source")
+
+%run "../includes/configuration"
+  
+%run "../includes/common_functions"
+
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
+from pyspark.sql.functions import col, lit
+
+# defining schema
+constructors_schema = "constructorId INT, constructorRef STRING, name STRING, nationality STRING, url STRING"
+
+# create dataframe of data file csv
+constructors_df = spark.read.schema(constructors_schema) \
+                        .json(f"{raw_folder_path}/constructors.json")
+
+constructors_dropped_df = constructors_df.drop(col("url"))
+
+constructors_renamed_df = constructors_dropped_df.withColumnRenamed("constructorId", "constructor_id") \
+                                  .withColumnRenamed("constructorRef", "constructor_ref") \
+                                  .withColumn("data_source", lit(v_data_source))
+
+constructors_final_df = add_ingestion_date(constructors_renamed_df)
+
+constructors_final_df.write.mode("overwrite").parquet("f"{processed_folder_path}/constructors")
+
+                                                
+                                    
