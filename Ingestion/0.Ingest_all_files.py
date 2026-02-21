96e1efe06c1be2bb2be3693e0bdecff846783d4f
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-21 12:20:19 +0530

    Update 0.Ingest_all_files.py

--- Ingestion/0.Ingest_all_files.py
+++ Ingestion/0.Ingest_all_files.py
@@ -1,1 +1,23 @@
+v_result = dbutils.notebook.run("circuits", 0, {"p_data_source" : "Ergast API"})
+v_result
 
+v_result = dbutils.notebook.run("constructors", 0, {"p_data_source" : "Ergast API"})
+v_result
+
+v_result = dbutils.notebook.run("drivers", 0, {"p_data_source" : "Ergast API"})
+v_result
+
+v_result = dbutils.notebook.run("lap_times", 0, {"p_data_source" : "Ergast API"})
+v_result
+
+v_result = dbutils.notebook.run("pit_stops", 0, {"p_data_source" : "Ergast API"})
+v_result
+
+v_result = dbutils.notebook.run("qualifying", 0, {"p_data_source" : "Ergast API"})
+v_result
+
+v_result = dbutils.notebook.run("races", 0, {"p_data_source" : "Ergast API"})
+v_result
+
+v_result = dbutils.notebook.run("results", 0, {"p_data_source" : "Ergast API"})
+v_result
