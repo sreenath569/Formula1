2b1c2009d35c7f42c4a148578c567a96a673b621
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-21 17:19:26 +0530

    Update race_results.py

--- transformations/race_results.py
+++ transformations/race_results.py
@@ -1,1 +1,20 @@
 
+%run "../includes/configuration"
+
+drivers_df = spark.read.parquet(f"{processed_folder_path}/drivers") \
+                        .withColumnRenamed("number", "driver_number") \
+                        .withColumnRenamed("name", "driver_name") \
+                        .withColumnRenamed("nationality", "driver_nationality")
+
+constructors_df = spark.read.parquet(f"{processed_folder_path}/constructors") \
+                            .withColumnRenamed("name", "team")
+
+circuits_df = spark.read.parquet(f"{processed_folder_path}/circuits") \
+                        .withColumnRenamed("location", "circuit_location")
+
+races_df = spark.read.parquet(f"{processed_folder_path}/races") \
+                    .withColumnRenamed("name", "race_name") \
+                    .withColumnRenamed("race_timestamp", "race_date")
+
+results_df = spark.read.parquet(f"{processed_folder_path}/results") \
+                        .withColumnRenamed("time", "race_time")
