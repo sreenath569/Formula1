7bb18558767a2e8443c0805a6471cbed46bc36b0
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-23 09:45:31 +0530

    Update race_results.py

--- transformations/race_results.py
+++ transformations/race_results.py
@@ -21,3 +21,14 @@ results_df = spark.read.parquet(f"{processed_folder_path}/results") \
 
 race_circuits_df = races_df.join(circuits_df, races_df.circuit_id==circuits_df.circuit_id, "inner") \
                             .select(races_df.race_id,races_df.race_year,races_df.race_name,races_df.race_date,circuits_df.circuit_location)
+
+race_results_df = results_df.join(race_circuits_df, results_df.race_id == race_circuits_df.race_id) \
+                            .join(drivers_df, results_df.driver_id == drivers_df.driver_id) \
+                            .join(constructors_df, results_df.constructor_id == constructors_df.constructor_id)
+
+final_df = race_results_df.select("race_year", "race_name", "race_date", "circuit_location", "driver_name", "driver_number", "driver_nationality", "team", "grid", "fastest_lap", "race_time", "points") \
+                          .withColumn("created_date", current_timestamp())
+
+display(final_df.filter("race_year == 2020 and race_name == 'Abu Dhabi Grand Prix'").orderBy(final_df.points.desc()))
+
+final_df.write.mode("overwrite").parquet(f"{presentation_folder_path}/race_results")
