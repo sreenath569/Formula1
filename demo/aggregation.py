84d7235a1d4cf648f6e426ba651f755d47661886
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2026-02-28 19:25:10 +0530

    Update aggregation.py

--- demo/aggregation.py
+++ demo/aggregation.py
@@ -13,6 +13,11 @@ race_results_df.select(countDistinct("race_name")).show()
 # sum
 race_results_df.select(sum("points")).show()
 
+race_results_df.select(sum("points"), countDistinct("race_name")) \
+                .withColumnRenamed("sum(points)", "total_points") \
+                .withColumnRenamed("count(Distinct race_name)", "number_of_races") \
+                .show()
+
                        
 
 
